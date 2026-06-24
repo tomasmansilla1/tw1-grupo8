fc710b40d4401c2db0d5f6f17295189efe41a2e2
LGava <lgavagnin074@alumno.unlam.edu.ar>
2026-06-24 16:20:04 -0300

    Buscar preguntas por categoria

--- src/main/java/com/tallerwebi/dominio/pregunta/PreguntaService.java
+++ src/main/java/com/tallerwebi/dominio/pregunta/PreguntaService.java
@@ -2,16 +2,10 @@ package com.tallerwebi.dominio.pregunta;
 
 import java.util.List;
 
-import org.springframework.stereotype.Service;
-
-@Service
 public interface PreguntaService {
-
     void guardar(Pregunta pregunta);
-
     List<Pregunta> listar();
-
     Pregunta obtenerPorId(Long id);
-
     void eliminar(Long id);
+    List<Pregunta> obtenerPorCategoria(String categoria);
 }
