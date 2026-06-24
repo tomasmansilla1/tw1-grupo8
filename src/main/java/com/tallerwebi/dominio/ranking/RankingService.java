07acb2bec716bfe012f6f0630d76e4a89b0e8a4f
LGava <lgavagnin074@alumno.unlam.edu.ar>
2026-06-24 16:22:31 -0300

    Arreglo funcion de calcular puntaje

--- src/main/java/com/tallerwebi/dominio/ranking/RankingService.java
+++ src/main/java/com/tallerwebi/dominio/ranking/RankingService.java
@@ -5,7 +5,5 @@ import com.tallerwebi.dominio.usuario.Usuario;
 
 public interface RankingService {
   // pide recibir un usuario y el puntaje base del juego actual
-
   Integer calcularPuntaje(Usuario usuario, Integer puntajeBase);
-  List<Usuario> obtenerTop10();
 }
--- src/main/java/com/tallerwebi/dominio/ranking/RankingServiceImpl.java
+++ src/main/java/com/tallerwebi/dominio/ranking/RankingServiceImpl.java
@@ -3,13 +3,8 @@ package com.tallerwebi.dominio.ranking;
 import org.springframework.stereotype.Service;
 
 import com.tallerwebi.dominio.usuario.Usuario;
-import com.tallerwebi.dominio.usuario.RepositoryUsuario;
-import org.springframework.beans.factory.annotation.Autowired;
-import javax.transaction.Transactional;
-import java.util.List;
 
 @Service
-@Transactional
 public class RankingServiceImpl implements RankingService {
 
   private RepositoryUsuario repositorioUsuario;
@@ -17,9 +12,16 @@ public class RankingServiceImpl implements RankingService {
   private static final int RESPUESTAS_PARA_BONUS = 3;
   private static final int BONUS_RACHA = 200;
 
-  @Autowired
-  public RankingServiceImpl(RepositoryUsuario repositorioUsuario) {
-    this.repositorioUsuario = repositorioUsuario;
+  @Override
+  public Integer calcularPuntaje(Usuario usuario,Integer puntajeBase) {
+
+    int nuevoPuntaje = puntajeBase;
+
+    if (usuario.getPartidasGanadasSeguidas() >= RESPUESTAS_PARA_BONUS) {
+      nuevoPuntaje += BONUS_RACHA;
+    }
+
+    return nuevoPuntaje;
   }
 
   @Override
